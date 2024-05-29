cd793d71ea695a1a419dbe1b158605b28647a21e
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-05-29 19:35:24 -0400

    Drew a triangle with colour

--- Main.cpp
+++ Main.cpp
@@ -2,14 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.02f, 1.0f);\n"
+"}\n\0";
 
+int main() {
+	//initialize GLFW
 	glfwInit();
 
+	//Tell GLFW what version of OpenGl we are using. --> we are using 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Tell GLFW we are using core profile, meaning we are only using the modern function.
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
+
+	//Creates window
 	GLFWwindow* window = glfwCreateWindow(1042, 796, "My OpenGl Game", NULL, NULL);
 
 	if (window == NULL) {
@@ -18,19 +40,77 @@ int main() {
 		return -1;
 	}
 
+	//adds the window to the current context
 	glfwMakeContextCurrent(window);
 
-	//add color to window
+	//setup gl viewport
 	gladLoadGL();
 	glViewport(0, 0, 1042, 796);
+
+	//creates shaders
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//creates shader program
+	GLuint shaderProgram = glCreateProgram();
+
+	//attach thoes two shaders above to the program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	//add color to window
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
 
 	while (!glfwWindowShouldClose(window)) {
+		//background color
+		glClearColor(0.10f, 0.13f, 0.37f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//init gl shader program
+		glUseProgram(shaderProgram);
+
+		//draw verteces
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//update window
+		glfwSwapBuffers(window);
+
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
